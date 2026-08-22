78ddfcccc460b984c2dcaa0a3a93b1a99a931b79
Neel🌊 <neelpatel.fr@gmail.com>
2026-08-22 21:03:54 +0530

    Add files via upload

---
+++ usd_to_inr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    float dollars, rupees;
+    float rate = 95.70; // Conversion rate
+
+    printf("Enter amount in Dollars ($): ");
+    scanf("%f", &dollars);
+
+    // Calculate currency conversion
+    rupees = dollars * rate;
+
+    // Display result with matching formatting
+    printf("\n=============================\n");
+    printf("%-20s$ %.2f\n", "Input Dollars:", dollars);
+    printf("%-20sRs. %.2f\n", "Indian Rupees:", rupees);
+    printf("=============================\n");
+}
